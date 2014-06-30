313bee8dc99b5e1f3e3c828566aabb8dc4c8cac4
sinius15 <sinius15@gmail.com>
2014-06-30 20:19:00 +0200

    auto commit on 30.06.2014 - 20:19:00

--- src/com/sinius15/cmdRep/commands/CmdCommand.java
+++ src/com/sinius15/cmdRep/commands/CmdCommand.java
@@ -1,5 +1,15 @@
 package com.sinius15.cmdRep.commands;
 
-public class CmdCommand {
+import com.sinius15.cmdRep.Command;
+
+public class CmdCommand extends Command{
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public void call() {}
 	
 }
--- src/com/sinius15/cmdRep/commands/SleepCommand.java
+++ src/com/sinius15/cmdRep/commands/SleepCommand.java
@@ -1,5 +1,15 @@
 package com.sinius15.cmdRep.commands;
 
-public class SleepCommand {
+import com.sinius15.cmdRep.Command;
+
+public class SleepCommand extends Command{
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public void call() {}
 	
 }
