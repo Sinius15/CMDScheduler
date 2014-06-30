331b98ccb625877d678a96554b47987c65e0b605
sinius15 <sinius15@gmail.com>
2014-06-30 20:03:53 +0200

    auto commit on 30.06.2014 - 20:03:53

--- src/com/sinius15/sceduler/GitScheduler.java
+++ src/com/sinius15/sceduler/GitScheduler.java
@@ -1,38 +1,9 @@
 package com.sinius15.sceduler;
 
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
-import javax.swing.JToggleButton;
-
-public class GitScheduler implements ActionListener, Runnable {
-	
-	private JTextArea txtArea;
-	private JToggleButton startStopBtn;
-	
-	private Thread runner;
-	
-	public GitScheduler() {
-
-		
-	}
-	
-
-	
-	
+public class GitScheduler{
 	
 	public static void main(String[] args) {
-		new GitScheduler();
+		new GitFrame();
 	}
 	
 }
