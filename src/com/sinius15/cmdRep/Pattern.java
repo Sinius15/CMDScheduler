e2438848803d5b49cd4008cd5a11415d56e4a08a
sinius15 <sinius15@gmail.com>
2014-06-30 20:31:56 +0200

    auto commit on 30.06.2014 - 20:31:56

--- src/com/sinius15/cmdRep/Pattern.java
+++ src/com/sinius15/cmdRep/Pattern.java
@@ -7,7 +7,7 @@ public abstract class Pattern {
 	public HashMap<String, Pattern> patterns = new HashMap<>();
 	
 	public Pattern(String name){
-		
+		patterns.put(name, this);
 	}
 	
 	public abstract String replace();
