ebf57294ad7ce31a4782aa2a07705b2d8e5cbe10
sinius15 <sinius15@gmail.com>
2014-06-30 20:30:26 +0200

    auto commit on 30.06.2014 - 20:30:26

--- src/com/sinius15/cmdRep/CMDRep.java
+++ src/com/sinius15/cmdRep/CMDRep.java
@@ -2,14 +2,21 @@ package com.sinius15.cmdRep;
 
 import com.sinius15.cmdRep.commands.CmdCommand;
 import com.sinius15.cmdRep.commands.SleepCommand;
+import com.sinius15.cmdRep.pattern.DatePattern;
+import com.sinius15.cmdRep.pattern.TimePattern;
 
 public class CMDRep{
 	
 	public static CMDRepFrame repFrame;
 	
 	public static void main(String[] args) {
+		//init commands
 		new CmdCommand();
 		new SleepCommand();
+		
+		//init patterns
+		new DatePattern();
+		new TimePattern();
 		
 		repFrame = new CMDRepFrame();
 	}
