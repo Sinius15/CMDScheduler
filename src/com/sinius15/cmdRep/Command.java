5990f9df9bff788585335740251bf9e6d9c5ced3
sinius15 <sinius15@gmail.com>
2014-06-30 20:27:04 +0200

    auto commit on 30.06.2014 - 20:27:04

--- src/com/sinius15/cmdRep/Command.java
+++ src/com/sinius15/cmdRep/Command.java
@@ -1,9 +1,13 @@
 package com.sinius15.cmdRep;
 
+import java.util.HashMap;
+
 public abstract class Command {
 	
+	public static HashMap<String, Command> commands = new HashMap<>();
+	
 	public Command(){
-		
+		commands.put(getName(), this);
 	}
 	
 	public abstract String getName();
