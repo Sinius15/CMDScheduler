3ede097c7a8c53036e46353336bcad062e2d441e
sinius15 <sinius15@gmail.com>
2014-06-30 20:40:21 +0200

    auto commit on 30.06.2014 - 20:40:21

--- src/com/sinius15/cmdRep/pattern/timePattern.java
+++ src/com/sinius15/cmdRep/pattern/timePattern.java
@@ -1,5 +1,8 @@
 package com.sinius15.cmdRep.pattern;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.sinius15.cmdRep.Pattern;
 
 public class TimePattern extends Pattern{
@@ -10,7 +13,7 @@ public class TimePattern extends Pattern{
 
 	@Override
 	public String replace() {
-		return null;
+		return new SimpleDateFormat("HH:mm:ss").format(new Date());
 	}
 	
 }
