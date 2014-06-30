d608a933e6aa8ab776242c0e0d33c3e000b1da07
sinius15 <sinius15@gmail.com>
2014-06-30 19:47:46 +0200

    auto commit on 30.06.2014 - 19:47:46

--- src/com/sinius15/sceduler/GitFrame.java
+++ src/com/sinius15/sceduler/GitFrame.java
@@ -2,10 +2,16 @@ package com.sinius15.sceduler;
 
 import java.awt.Frame;
 
+import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
+
 public class GitFrame extends Frame{
 
 	private static final long serialVersionUID = 1L;
 	
+	private JTextArea txtArea;
+	private JToggleButton startStopBtn;
 	
+	public GitFrame
 	
 }
