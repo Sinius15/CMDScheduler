1f7258b1ed7366f32c3ed947ec1f4f0b0160b7c2
sinius15 <sinius15@gmail.com>
2014-06-30 20:36:59 +0200

    auto commit on 30.06.2014 - 20:36:59

--- src/com/sinius15/cmdRep/commands/SleepCommand.java
+++ src/com/sinius15/cmdRep/commands/SleepCommand.java
@@ -1,5 +1,9 @@
 package com.sinius15.cmdRep.commands;
 
+import javax.swing.JOptionPane;
+
+import com.sinius15.cmdRep.CMDRep;
+import com.sinius15.cmdRep.CMDRepFrame;
 import com.sinius15.cmdRep.Command;
 
 public class SleepCommand extends Command{
@@ -10,7 +14,22 @@ public class SleepCommand extends Command{
 
 	@Override
 	public void call(String line) {
-		System.out.println("SLEEP!");
+		line = line.replace("sleep", "").trim();
+		int sleepTime;
+		try {
+			sleepTime = Integer.parseInt(line);
+		} catch (NumberFormatException e) {
+			CMDRepFrame.errLog("Fatal Error!!");
+			JOptionPane.showMessageDialog(CMDRep.repFrame,
+					"Invalid argument." + System.lineSeparator()
+							+ "After 'sleep ' there must be a number!", "Fatal Error",
+					JOptionPane.ERROR_MESSAGE);
+			break;
+		}
+		try {
+			CMDRepFrame.log("Sleeping for " + sleepTime + "ms");
+			Thread.sleep(sleepTime);
+		} catch (InterruptedException e) {}
 	}
 	
 }
