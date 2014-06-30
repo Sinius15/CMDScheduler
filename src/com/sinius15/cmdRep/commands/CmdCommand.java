791468f5a39c7e11e1dba39865c7b1d5e283e8e6
sinius15 <sinius15@gmail.com>
2014-06-30 20:39:20 +0200

    auto commit on 30.06.2014 - 20:39:20

--- src/com/sinius15/cmdRep/commands/CmdCommand.java
+++ src/com/sinius15/cmdRep/commands/CmdCommand.java
@@ -1,5 +1,13 @@
 package com.sinius15.cmdRep.commands;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.sinius15.cmdRep.CMDRepFrame;
 import com.sinius15.cmdRep.Command;
 
 public class CmdCommand extends Command{
@@ -9,8 +17,27 @@ public class CmdCommand extends Command{
 	}
 
 	@Override
-	public void call(String line) {
-		System.out.println("CMD!");
+	public boolean call(String line) {
+		curLine = curLine.replace("cmd", "").trim();
+		curLine = curLine.replace("%time", new SimpleDateFormat("HH:mm:ss").format(new Date()));
+		curLine = curLine.replace("%date", new SimpleDateFormat("dd.MM.yyy").format(new Date()));
+		
+		
+		ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", curLine);
+		builder.directory(new File(System.getProperty("user.dir")));
+		builder.redirectErrorStream(true);
+		try {
+			Process p = builder.start();
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
+			String line;
+			while ((line = r.readLine()) != null) {
+				CMDRepFrame.errLog(line);
+			}
+			r.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
 	}
 	
 }
