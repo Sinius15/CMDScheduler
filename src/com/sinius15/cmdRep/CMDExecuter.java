3ed9147666dcc107b7012c323dd9f0ac366a033e
sinius15 <sinius15@gmail.com>
2014-06-30 20:41:31 +0200

    auto commit on 30.06.2014 - 20:41:31

--- src/com/sinius15/cmdRep/CMDExecuter.java
+++ src/com/sinius15/cmdRep/CMDExecuter.java
@@ -27,37 +27,13 @@ public class CMDExecuter implements Runnable{
 		while (CMDRepFrame.isRunning) {
 			String curLine = lines[i] + " ";
 			
-			String commandName = curLine.split(" ")[0];
+			
+			
+			String[] words = curLine.split(" ")[0];
 			
 			Command c = Command.commands.get(commandName);
 			if(c.call(curLine) == false)
 				break;  //fatal error;
-			
-			
-			if (curLine.toLowerCase().startsWith("sleep")) {   // sleep!!!
-				
-			} else if (curLine.toLowerCase().startsWith("cmd")) { // cmd!!!
-				curLine = curLine.replace("cmd", "").trim();
-				curLine = curLine.replace("%time", new SimpleDateFormat("HH:mm:ss").format(new Date()));
-				curLine = curLine.replace("%date", new SimpleDateFormat("dd.MM.yyy").format(new Date()));
-				
-				
-				ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", curLine);
-				builder.directory(new File(System.getProperty("user.dir")));
-				builder.redirectErrorStream(true);
-				try {
-					Process p = builder.start();
-					BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
-					String line;
-					while ((line = r.readLine()) != null) {
-						CMDRepFrame.errLog(line);
-					}
-					r.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-			}
 			
 			i++;
 			if (i == lines.length)
