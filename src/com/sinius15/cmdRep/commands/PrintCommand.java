1aebca6d2fbd565e52f1aee24b2c645dcaa67ba9
sinius15 <sinius15@gmail.com>
2014-07-01 13:26:48 +0200

    auto commit on 01.07.2014 - 13:26:48

--- src/com/sinius15/cmdRep/commands/PrintCommand.java
+++ src/com/sinius15/cmdRep/commands/PrintCommand.java
@@ -1,6 +1,6 @@
 package com.sinius15.cmdRep.commands;
 
-import com.sinius15.cmdRep.CMDRep;
+import com.sinius15.cmdRep.CMDRepFrame;
 import com.sinius15.cmdRep.Command;
 
 public class PrintCommand extends Command{
@@ -12,7 +12,7 @@ public class PrintCommand extends Command{
 	@Override
 	public boolean call(String line) {
 		line = line.replace("print", "").trim();
-		CMDRep.repFrame.log(line);
+		CMDRepFrame.log(line);
 		return true;
 	}
 	
