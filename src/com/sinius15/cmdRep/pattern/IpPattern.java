537de0f53914943440df78731daa1783225cb7f8
sinius15 <sinius15@gmail.com>
2014-07-01 13:43:26 +0200

    auto commit on 01.07.2014 - 13:43:26

--- src/com/sinius15/cmdRep/pattern/IpPattern.java
+++ src/com/sinius15/cmdRep/pattern/IpPattern.java
@@ -1,5 +1,19 @@
 package com.sinius15.cmdRep.pattern;
 
-public class IpPattern {
+import com.sinius15.cmdRep.Pattern;
+
+public class IpPattern extends Pattern{
+
+	@Override
+	public String getName() {
+		return "ip";
+	}
+
+	@Override
+	public String replace() {
+		return null;
+	}
+	
+	
 	
 }
